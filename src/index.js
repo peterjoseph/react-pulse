ae78b537f98b0f3512fffc9fd81b9075a7e7fc64
Peter Joseph <contact@peterjoseph.net>
2018-02-19 07:12:04 +0800

    Mandatory default component files

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import style from "./styles.css";
+
+class Pulse extends React.Component {
+  render() {
+    return <div/>
+  }
+}
+
+Pulse.defaultProps = {
+};
+
+Pulse.propTypes = {
+};
+
+export default Pulse;
