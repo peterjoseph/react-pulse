5d3f17ecbb68c2aff90603a5f8e6972affcbfc19
Peter Joseph <contact@peterjoseph.net>
2018-02-20 06:46:42 +0800

    Added props to component

--- src/index.js
+++ src/index.js
@@ -3,15 +3,60 @@ import PropTypes from "prop-types";
 import style from "./styles.css";
 
 class Pulse extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: false,
+    };
+
+    this.preparePulse = this.preparePulse.bind(this);
+  }
+
+  componentWillMount() {
+    this.setState({
+      active: this.props.defaultActive ? "false" : "true",
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+  }
+
+  preparePulse() {
+    this.setState({
+      active: this.state.active ? "false" : "true",
+    });
+  }
+
   render() {
-    return <div/>
+    const { active } = this.state;
+    const { defaultLabel, activeLabel, className } = this.props;
+
+    return <button className={className} onClick={this.preparePulse}>
+      {active ? activeLabel : defaultLabel }
+    </button>
   }
 }
 
 Pulse.defaultProps = {
+  defaultLabel: "Start",
+  activeLabel: "Running",
+  pulseTime: 30,
+  showTimer: false,
+  disabled: false
 };
 
 Pulse.propTypes = {
+  defaultLabel: Proptypes.string,
+  activeLabel: PropTypes.string,
+  defaultActive: PropTypes.bool,
+  pulseFunction: PropTypes.func,
+  pulseTime: PropTypes.number, // Seconds
+  onClickFunction: PropTypes.func,
+  onChangeFunction: PropTypes.func,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+  showTimer: PropTypes.bool,
 };
 
 export default Pulse;
